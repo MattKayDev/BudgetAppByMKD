648a3829a57889775a720132c6204b27d8e72ea0
Matt Kay (Maciej Kolacinski) <maciek200895@gmail.com>
2023-06-02 17:54:40 +0100

    Created PayItem service and moved some of the PayItem methods to it from the List Page

---
+++ MauiBudgetApp/Services/PayItemService.cs
@@ -0,0 +1,78 @@
+﻿using MauiBudgetApp.Data;
+using MauiBudgetApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiBudgetApp.Services
+{
+    public class PayItemService
+    {
+        List<PayItem> payItems = new();
+        private int itemCount = 0;
+        public int ItemCount
+        {
+            get => this.itemCount;
+            set
+            {
+                if (this.itemCount == value)
+                {
+                    return;
+                }
+
+                var count = Task.Run(async () =>
+                {
+                    var items = await this.GetPayItemsAsync();
+                    return items.Count;
+                });
+
+                this.itemCount = count.Result;
+            }
+        }
+
+        public decimal GetTotalFor(List<PayItem> payItems)
+        {
+            if (payItems.Count > 0)
+            {
+                var total = payItems.Sum(i => i.Amount);
+                return total;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public decimal GetLeftToPay(List<PayItem> payItems)
+        {
+            if (!payItems.All(i => i.IsExpense))
+            {
+                return 0;
+            }
+
+            var leftToPay = payItems.Where(i => i.IsPaid).ToList().Sum(a => a.Amount);
+            return leftToPay;
+        }
+
+        public async Task<List<PayItem>> GetExpenseItemsAsync()
+        {
+            var items = await this.GetPayItemsAsync();
+            return items.Where<PayItem>(i => i.IsExpense).ToList();
+        }
+
+        public async Task<List<PayItem>> GetIncomeItemsAsync()
+        {
+            var items = await this.GetPayItemsAsync();
+            return items.Where<PayItem>(i => i.IsIncome).ToList();
+        }
+
+        private async Task<List<PayItem>> GetPayItemsAsync()
+        {
+            PayItemItemDatabase database = await PayItemItemDatabase.Instance;
+            this.payItems = await database.GetItemsAsync();
+            return this.payItems;
+        }
+    }
+}

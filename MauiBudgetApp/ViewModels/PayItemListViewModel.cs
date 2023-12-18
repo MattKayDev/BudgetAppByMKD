0e05515acda34c2576ce725cf2425fe150a95adc
Maciej Kolacinski <hello@mattkaydev.com>
2023-12-18 00:18:45 +0000

    Created PayItemListViewModel but nothing is being done with it yet

---
+++ MauiBudgetApp/ViewModels/PayItemListViewModel.cs
@@ -0,0 +1,21 @@
+﻿using MauiBudgetApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiBudgetApp.ViewModels
+{
+    public class PayItemListViewModel : BaseViewModel
+    {
+
+        public PayItemListViewModel(PayItemService payItemService)
+        {
+            this.Title = "";
+            this.PayItemService = payItemService;
+        }
+
+
+    }
+}
